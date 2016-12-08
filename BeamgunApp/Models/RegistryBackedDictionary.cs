47a27c776b955d96f51094362c627bd13f7f9ef9
Josh Lospinoso <josh@lospi.net>
2016-12-08 08:06:11 -0600

    Fixes Issue #10, using TryParse on each type to pull registry entries)

--- BeamgunApp/Models/RegistryBackedDictionary.cs
+++ BeamgunApp/Models/RegistryBackedDictionary.cs
@@ -7,7 +7,9 @@ namespace BeamgunApp.Models
     {
         Guid GetWithDefault(string key, Guid defaultValue);
         bool GetWithDefault(string key, bool defaultValue);
-        T GetWithDefault<T>(string key, T defaultValue);
+        string GetWithDefault(string key, string defaultValue);
+        uint GetWithDefault(string key, uint defaultValue);
+        double GetWithDefault(string key, double defaultValue);
         void Set<T>(string key, T value);
     }
 
@@ -15,18 +17,34 @@ namespace BeamgunApp.Models
     {
         public Action<string> BadCastReport;
         public const string BeamgunBaseKey = "HKEY_CURRENT_USER\\SOFTWARE\\Beamgun";
-        
-        public T GetWithDefault<T>(string key, T defaultValue)
+
+        public string GetWithDefault(string key, string defaultValue)
         {
             try
             {
-                return (T) Registry.GetValue(BeamgunBaseKey, key, defaultValue);
+                return (string)Registry.GetValue(BeamgunBaseKey, key, defaultValue);
             }
             catch (InvalidCastException)
             {
                 BadCastReport($"Could not parse {BeamgunBaseKey} {key}. Using default {defaultValue}.");
                 return defaultValue;
             }
+        }
+
+        public double GetWithDefault(string key, double defaultValue)
+        {
+            double result;
+            return double.TryParse(Registry.GetValue(BeamgunBaseKey, key, defaultValue).ToString(), out result)
+                ? result
+                : defaultValue;
+        }
+        
+        public uint GetWithDefault(string key, uint defaultValue)
+        {
+            uint result;
+            return uint.TryParse(Registry.GetValue(BeamgunBaseKey, key, defaultValue).ToString(), out result)
+                ? result
+                : defaultValue;
         }
 
         public Guid GetWithDefault(string key, Guid defaultValue)
